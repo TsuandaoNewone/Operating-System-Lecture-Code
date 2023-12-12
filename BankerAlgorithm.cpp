c616aa631ad2eb5b03d9483cf8d9149b58bfc679
川岛新一 <805783115@qq.com>
2023-12-12 17:23:06 +0800

    Add files via upload

---
+++ BankerAlgorithm.cpp
@@ -0,0 +1,165 @@
+﻿#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int resourceSize = 3;
+int processNum;
+int* resourceTotal;
+int** MAX;
+int** ALLOCATE;
+int** NEED;
+int processDoneCnt;
+vector<int> safeSequence;
+vector<int> allocateSequence;
+bool* processDone;
+// 函数：数组加法
+void arrayAdd(const int* arr1, const int* arr2, int* result, int size) {
+	for (int i = 0; i < size; ++i) {
+		result[i] = arr1[i] + arr2[i];
+	}
+}
+// 函数：数组减法
+void arraySubtract(const int* arr1, const int* arr2, int* result, int size) {
+	for (int i = 0; i < size; ++i) {
+		result[i] = arr1[i] - arr2[i];
+	}
+}
+
+// 显示数组的内容
+void display() {
+	printf("Total resource: ");
+	for (int j = 0; j < resourceSize; j++) {
+		cout << resourceTotal[j] << " ";
+	}
+	cout << endl;
+	for (int i = 0; i < processNum; i++) {
+		printf("Process %d   MAX: ", i);
+		for (int j = 0; j < resourceSize; j++) {
+			cout << MAX[i][j] << " ";
+		}
+		printf("  ALLOCATE: ");
+		for (int j = 0; j < resourceSize; j++) {
+			cout << ALLOCATE[i][j] << " ";
+		}
+		printf("  NEED: ");
+		for (int j = 0; j < resourceSize; j++) {
+			cout << NEED[i][j] << " ";
+		}
+		processDone[i] ? printf("   finished") : printf("   unfinished");
+		cout << endl;
+	}
+	//cout << endl;
+}
+
+bool noBigger(const int* A, const int* B, const int size) {
+	for (int i = 0; i < size; i++) {
+		if (A[i] > B[i])
+			return false;
+	}
+	return true;
+}
+
+bool safetyCheck(int i) {
+	safeSequence.clear();
+	bool* tempProcessDone = new bool[processNum];
+	int* tempResourceTotal = new int[resourceSize];
+	copy(processDone, processDone + processNum, tempProcessDone);
+	copy(resourceTotal, resourceTotal + resourceSize, tempResourceTotal);
+	int checkCnt = 0;
+	bool over = false;
+	while (!over) {
+		over = true;
+		for (int j = 0; j < processNum; j++) {
+			if (!tempProcessDone[j] && noBigger(NEED[j], tempResourceTotal, resourceSize)) {
+				tempProcessDone[j] = true;
+				over = false;
+				arrayAdd(tempResourceTotal, ALLOCATE[j], tempResourceTotal, resourceSize);
+				safeSequence.push_back(j);
+				checkCnt++;
+			}
+		}
+	}
+	if (checkCnt + processDoneCnt == processNum)
+		return true;
+	return false;
+}
+
+int main() {
+	ifstream inputFile("input.txt");
+	inputFile >> processNum >> resourceSize;
+
+
+	resourceTotal = new int[resourceSize];
+	processDone = new bool[processNum];
+	MAX = new int* [processNum];
+	ALLOCATE = new int* [processNum];
+	NEED = new int* [processNum];
+
+	memset(processDone, false, sizeof(processDone));
+	for (int i = 0; i < resourceSize; i++) {
+		inputFile >> resourceTotal[i];
+	}
+	for (int i = 0; i < processNum; i++) {
+		int id;
+		inputFile >> id;
+		MAX[i] = new int[resourceSize];
+		ALLOCATE[i] = new int[resourceSize];
+		NEED[i] = new int[resourceSize];
+		for (int j = 0; j < resourceSize; j++) {
+			inputFile >> MAX[i][j];
+		}
+		for (int j = 0; j < resourceSize; j++) {
+			inputFile >> ALLOCATE[i][j];
+		}
+		for (int j = 0; j < resourceSize; j++) {
+			inputFile >> NEED[i][j];
+		}
+	}
+	display();
+	cout << endl;
+	bool over = false;
+	while(!over) {
+		over = true;
+		for (int i = 0; i < processNum; i++) {
+			if (processDone[i]) continue;
+			printf("process %d request resources,", i);
+			//undone and allocatable
+			if (noBigger(NEED[i], resourceTotal, resourceSize)) {
+				printf(" enough resources, try allocating\n");
+				// try allocating / 
+				arrayAdd(ALLOCATE[i], resourceTotal, resourceTotal, resourceSize);
+				processDone[i] = true;
+				processDoneCnt++;
+				//allocate success
+				if (safetyCheck(i)) {
+					printf("safe sequence found: ");
+					allocateSequence.push_back(i);
+					for (auto j : allocateSequence)
+						printf("%d ", int(j));
+					for (auto j : safeSequence)
+						printf("%d ", int(j));
+					copy(MAX[i], MAX[i] + resourceSize, ALLOCATE[i]);
+					memset(NEED[i], 0, sizeof(NEED[i]));
+					display();
+					cout << endl;
+					over = false;
+				}
+				//no safe sequence, allocate fail, recall resources
+				else {
+					printf("safe sequence not found, request rejected\n");
+					arraySubtract(resourceTotal, ALLOCATE[i], resourceTotal, resourceSize);
+					processDone[i] = false;
+					processDoneCnt--;
+				}
+			}
+			else {
+				printf("not enough resources,request failed\n");
+			}
+		}
+	}
+	if (processDoneCnt == processNum) {
+		printf("All processes successfully ran\n");
+	}
+		return 0;
+}
---
+++ reader_first.cpp
@@ -0,0 +1,107 @@
+﻿#include <iostream>
+#include <string>
+#include <thread>
+#include <mutex>
+#include <fstream>
+#include <vector>
+#include <condition_variable>
+#include <algorithm>
+#define maxThreads 100
+using namespace std;
+void reader(int id, char userType, double startTime, double endureTiem);
+void writer(int id, char userType, double startTime, double endureTiem);
+condition_variable cv;
+mutex mtx;
+bool write = false;
+bool ready = false;
+int readerCnt = 0;
+struct USER {
+	int id;
+	double startTime;
+	USER(int id_, double startTime_) : id(id_), startTime(startTime_) {}
+};
+vector<USER> writers;
+bool compare(USER a, USER b) {
+	return a.startTime > b.startTime;
+}
+thread threads[maxThreads];
+int main()
+{
+	ifstream inputFile("input.txt");
+	if (!inputFile.is_open()) {
+		cerr << "Failed to open the file.\n";
+		return 1;
+	}
+	int cnt = 0;
+	int id;
+	char userType;
+	double startTime, endureTime;
+	while (inputFile >> id >> userType >> startTime >> endureTime) {
+		printf("%d %c %f %f\n", id, userType, startTime, endureTime);
+		if (userType == 'R') {
+			threads[cnt++] = thread(reader, id, userType, startTime, endureTime);
+		}
+		else {
+			threads[cnt++] = thread(writer, id, userType, startTime, endureTime);
+			writers.push_back(USER(id, startTime));
+		}
+	}
+	sort(writers.begin(), writers.end(), compare);
+	{
+		lock_guard<std::mutex> lock(mtx);
+		// threads all created, wake them up
+		ready = true;
+		cv.notify_all();
+	}
+	//check before join a thread
+	for (auto& a : threads) {
+		if (a.joinable())
+			a.join();
+	}
+	return 0;
+}
+void reader(int id, char userType, double startTime, double endureTime) {
+	{
+		unique_lock<mutex> lk(mtx);
+		cv.wait(lk, [] {return ready; });
+	}
+	this_thread::sleep_for(chrono::milliseconds((long long)(startTime * 1000)));
+	printf("reader %d request to read\n", id);
+	//stuck until no one's writing
+	while (true) {
+		//reader-first requirements:
+		if (!write || readerCnt > 0) {
+			lock_guard<mutex> lk(mtx);
+			readerCnt++;
+			printf("reader %d is reading\n", id);
+			break;
+		}
+	}
+	this_thread::sleep_for(chrono::milliseconds((long long)(endureTime * 1000)));
+	{
+		lock_guard<mutex> lk(mtx);
+		readerCnt--;
+		printf("reader %d finished reading\n", id);
+	}
+}
+void writer(int id, char userType, double startTime, double endureTiem) {
+	{
+		unique_lock<mutex> lk(mtx);
+		cv.wait(lk, [] {return ready; });
+	}
+	this_thread::sleep_for(chrono::milliseconds((long long)(startTime * 1000)));
+	printf("writer %d request to write\n", id);
+	while (true) {
+		lock_guard<mutex> lk(mtx);
+		if (readerCnt == 0 && writers.back().id == id) {
+			write = true;
+			printf("writer %d is writing\n", id);
+			this_thread::sleep_for(chrono::milliseconds((long long)(startTime * 1000)));
+			write = false;
+			printf("writer %d finished writing\n", id);
+			if (!writers.empty())
+				writers.pop_back();
+			break;
+		}
+	}
+}
